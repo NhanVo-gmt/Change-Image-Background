bfe35302524c82b707ac807624d83f53a989aee2
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-28 14:45:55 +1100

    adding white background to image

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import cv2 as cv
 
+# variable
+RED = 255
+GREEN = 255
+BLUE = 255
+ALPHA = 255
+
+Image = cv.imread("1.png", cv.IMREAD_UNCHANGED)
+
+trans_mask = Image[:, :, 3] == 0
+
+Image[trans_mask] = [BLUE, GREEN, RED, ALPHA]
+print(Image.shape)
+
+resized = cv.resize(Image, None, fx=0.1, fy=0.1)
+cv.imshow('windows', resized)
+cv.waitKey(0)
